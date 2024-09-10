59e88838c6b4bd92b64fe9d14522fc19adde606a
Lazaro Menezes <lazarodm@gmail.com>
2024-09-10 16:58:20 -0300

    Empty project

---
+++ ElfCalories.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ElfCalories.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ElfCalories.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ElfCalorires.Core;
+
+public class Class1
+{
+
+}
